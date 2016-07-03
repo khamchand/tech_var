391449da37a66ce656e9c5fd74a0b63bf1f5a86c
Khamchand Saha <Khamchand Saha>
2016-07-03 22:11:53 +0530

    student class for Group class

---
+++ Student.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Khamchand Saha
+ */
+public class Student {
+    private String name;
+    private int roll_no;
+    private int age;
+    private int score;
+    public void setName(String nam)
+    {
+        name = nam;
+    }    
+    public String getName()
+    {
+        return name;
+    }
+    public void setRoll_no(int a)
+    {
+        roll_no = a;
+    }    
+    public int getRoll_no()
+    {
+        return roll_no;
+    }
+    public void setAge(int b)
+    {
+        age = b;
+    }    
+    public int getAge()
+    {
+        return age;
+    }
+    public void setScore(int s)
+    {
+        score = s;
+    }    
+    public int getScore()
+    {
+        return score;
+    }
+}
